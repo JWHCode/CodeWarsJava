adf8041fc2c2830c14c1f1c4f18f5f0d0700fa1d
JWHCode <45348787+JWHCode@users.noreply.github.com>
2022-03-22 22:39:52 -0400

    Create CodeWars.java

---
+++ CodeWars.java
@@ -0,0 +1,12 @@
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+*/
+
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    if (numbers.length != 10) // no error handling was specified in the description, so
+      return null;
+    return String.format("(%d%d%d) %d%d%d-%d%d%d%d", 
+                         numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+    } // demonstrating string interpolation seemed the most straight forward approach since the size of the array is static.
+  }
