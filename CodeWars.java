110afafdbd49467e8b3b348cdaa6aa49f9c06848
JWHCode <45348787+JWHCode@users.noreply.github.com>
2022-03-22 22:40:44 -0400

    Update CodeWars.java

--- CodeWars.java
+++ CodeWars.java
@@ -10,3 +10,25 @@ public class Kata {
                          numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
     } // demonstrating string interpolation seemed the most straight forward approach since the size of the array is static.
   }
+
+/*
+Digital root is the recursive sum of all the digits in a number.
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+The input will be a non-negative integer.
+ */
+public class DigitalRoot {
+	  public static int digital_root(int n) {
+		  if (n > 0 && n < 10) 
+			  return n;
+		  int sum = 0;
+		  while (n > 0) {
+			  sum =  sum + (n % 10);
+			  n = n / 10;
+		  }
+		  if (sum >= 10) {
+			  return digital_root(sum);
+		  }
+		  else
+			  return sum;
+	  }
+}
