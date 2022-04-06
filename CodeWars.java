de467d3e270cb4171157571a6bfcd4977940bea0
JWHCode <45348787+JWHCode@users.noreply.github.com>
2022-04-06 15:31:16 -0400

    Update CodeWars.java

--- CodeWars.java
+++ CodeWars.java
@@ -1,4 +1,45 @@
 /*
+You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. 
+The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). 
+You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. 
+Return false otherwise.
+*/
+
+public class TenMinWalk {
+  public static boolean isValid(char[] walk) {
+    if (walk.length != 10) { // has to be exactly 10 in length due to specification
+      return false;
+    }
+    int n = 0;
+    int s = 0;
+    int e = 0;
+    int w = 0;
+    for (char c : walk) {
+      switch (Character.toLowerCase(c)) {
+          case 'n': 
+            n++;
+            break;
+          case 's':
+            s++;
+            break;
+          case 'e':
+            e++;
+            break;
+          case 'w':
+            w++;
+          break;  
+      }
+    }
+    if (n - s == 0 && e - w == 0) {  // If we want to end up where we were, then the difference of opposites must = 0
+      return true;
+      }
+    else {
+      return false;
+    }
+  }
+
+/*
 Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 Examples
 Valid arrays
